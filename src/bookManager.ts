cd8e40270f1192f543315fba421ac961683d6a82
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2021-01-10 16:07:20 +0900

    feat:robots.txt parsing

--- src/bookManager.ts
+++ src/bookManager.ts
@@ -1,22 +1,33 @@
 import { Book } from "./book";
 import { Parser } from "./parser";
+import axios from "axios";
 
 export interface BookManager {
   find: (parser: Parser) => Promise<Book>;
 }
 
 export class Booklog implements BookManager {
+  private readonly host = "https://booklog.jp";
+  private readonly params =
+    "?category_id=all&status=3&rank=all&sort=read_desc&display=front";
 
   /**
    * Find latest book user read
-   * @param parser 
+   * @param parser
    */
-  find(parser: Parser): Promise<Book> {
-    const host = "https://booklog.jp";
-    const params =
-      "?category_id=all&status=3&rank=all&sort=read_desc&display=front";
-    const url = `${host}/users/${process.env.BOOKLOG_USER_ID}/all${params}`;
-
+  async find(parser: Parser): Promise<Book> {
+    const url = `${this.host}/users/${process.env.BOOKLOG_USER_ID}/all${this.params}`;
+    if (await this.isDisAllowedScraping()) {
+      throw new Error("Unable to parse");
+    }
     return parser.parse(url);
+  }
+
+  private async isDisAllowedScraping(): Promise<boolean> {
+    const { data: robotsTxt } = await axios.get<string>(
+      `${this.host}/robots.txt`
+    );
+
+    return new RegExp("users/.*.all").test(robotsTxt);
   }
 }
